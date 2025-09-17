1a828a8ee624e4748707cd9f908a08781b575dcd
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-09-18 00:03:50 +0530

    Time: 1 ms (60.90%) | Memory: 41.9 MB (32.13%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,22 +1,43 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 1) return strs[0];
+        if (strs.length == 0) return "";
+        if (strs.length == 1) return strs[0];
+
         Arrays.sort(strs);
         String s1 = strs[0];
         String s2 = strs[strs.length - 1];
-        String res = "";
-        int x = 0;
-        int y = 0;
-        while(x < s1.length() && y < s2.length()){
-            if(s1.charAt(x) == s2.charAt(y)){
-                res += s1.charAt(x);
-                x++;
-                y++;
-            }else{
+
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < Math.min(s1.length(), s2.length()); i++) {
+            if (s1.charAt(i) == s2.charAt(i)) {
+                res.append(s1.charAt(i));
+            } else {
                 break;
             }
         }
-        // if(ans.length() == 0) return -1;
-        return res;
+        return res.toString();
     }
 }
+
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         if(strs.length == 1) return strs[0];
+//         Arrays.sort(strs);
+//         String s1 = strs[0];
+//         String s2 = strs[strs.length - 1];
+//         String res = "";
+//         int x = 0;
+//         int y = 0;
+//         while(x < s1.length() && y < s2.length()){
+//             if(s1.charAt(x) == s2.charAt(y)){
+//                 res += s1.charAt(x);
+//                 x++;
+//                 y++;
+//             }else{
+//                 break;
+//             }
+//         }
+//         // if(ans.length() == 0) return -1;
+//         return res;
+//     }
+// }
