c1cf03270f219c428190a3166bca0b346bcf1b70
Saurabh Kumar Singh <156588843+saurabhsingh223@users.noreply.github.com>
2025-12-26 23:19:19 +0530

    Time: 46 ms (84.36%) | Memory: 54.4 MB (70.74%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,14 @@
+var timeLimit = function (fn, t) {
+    return async function (...args) {
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+        });
+
+        return Promise.race([
+            fn(...args),
+            timeoutPromise
+        ]);
+    };
+};
